b69c486e6d33d8f8aadc775d4b5cc169a672ef39
mkrivic83 <krivic.milan@gmail.com>
2025-12-03 18:53:57 +0100

    insert kategorije

--- Models/Kategorija.php
+++ Models/Kategorija.php
@@ -3,7 +3,35 @@ require_once __DIR__.'..\..\DB\DB.php';
 require_once "Redirect.php";
 class Kategorija{
 
+    public int $id;
+    public string $naziv;
 
+    public static function allCategories(): array{
+        $db = DB::getInstance()->connpdo;
+        $sql = "SELECT * FROM kategorije";
+        $result = $db->query($sql);
+
+        return $result->fetchAll();
+    }
+
+    public static function insert($naziv){
+        $db = DB::getInstance()->connpdo;
+
+        try {
+            $sql="INSERT INTO kategorije (naziv) values (:naziv)";
+            $stmt = $db->prepare($sql);
+
+            $ok = $stmt->execute(
+                [
+                    ':naziv'=>$naziv
+                ]
+                );
+            return $ok;
+        } catch (PDOException $e) {
+            $msg=$e->getMessage();
+            Redirect::redirectToErrorPage($msg);
+        }
+    }
     
 }
 
