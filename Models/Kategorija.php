8679789c140b703af026f35674b8002303b326bf
mkrivic83 <krivic.milan@gmail.com>
2025-12-03 19:14:29 +0100

    update i brisanje kategorije

--- Models/Kategorija.php
+++ Models/Kategorija.php
@@ -32,6 +32,45 @@ class Kategorija{
             Redirect::redirectToErrorPage($msg);
         }
     }
+
+    public static function getById($id){
+        $db = DB::getInstance()->connpdo;
+
+        $stmt = $db->prepare("SELECT * FROM kategorije where id = :id");
+        $stmt->execute([':id'=>$id]);
+
+        $row = $stmt->fetch();
+        return $row ?: null;
+    }
+
+    public static function update($id,$naziv){
+        $db = DB::getInstance()->connpdo;
+
+        $sql="UPDATE kategorije set naziv = :naziv where id=:id";
+        $stmt = $db->prepare($sql);
+
+        $ok = $stmt->execute([
+            ':id'=>$id,
+            ':naziv'=>$naziv
+        ]);
+
+        return $ok;
+    }
+
+    public static function delete($id){
+        $db = DB::getInstance()->connpdo;
+        try{
+            $sql = "DELETE from kategorije where id = :id";
+            $stmt = $db->prepare($sql);
+            $_SESSION["poruka"]="Kategorija uspješno izbrisana";
+            return $stmt->execute([':id'=>$id]);
+        }
+        catch(PDOException $e){
+            $msg="Greška: ".$e->getMessage();
+            Redirect::redirectToErrorPage($msg);
+            exit;
+        }
+    }
     
 }
 
